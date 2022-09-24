3bec1f190aaef55db5d4070e25446e5c39e5fb56
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-25 00:12:56 +0100

    Implementation changed

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from lib2to3.pgen2 import driver
 from selenium import webdriver
 
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time as sl
 
 service_obj = Service("D:/chromedriver/chromedriver")
 driver = webdriver.Chrome(service=service_obj)
 
+driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.find_element(By.CSS_SELECTOR, value="input[type='search']").send_keys('ber')
+sl.sleep(7)
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+assert count > 0
+for result in results:
+    button = result.find_element(By.XPATH, './div/button').click()
+    
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
