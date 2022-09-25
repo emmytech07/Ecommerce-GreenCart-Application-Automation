ce941e213b33a7cd27a7735f0131316e89010f8b
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-25 02:00:02 +0100

    all tests cases pass so far

--- test_expected.py
+++ test_expected.py
@@ -6,7 +6,8 @@ import time as sl
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+expectedList =['Cucumber - 1 Kg','Raspberry - 1/4 Kg', "Strawberry - 1/4 Kg"]
+actualList = []
 service_obj = Service("D:/chromedriver/chromedriver")
 driver = webdriver.Chrome(service=service_obj)
 driver.implicitly_wait(5)
@@ -15,11 +16,19 @@ driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.CSS_SELECTOR, value="input[type='search']").send_keys('ber')
 sl.sleep(4)
+
+
 results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 count = len(results)
 assert count > 0
+
 for result in results:
+    productName = result.find_element(By.XPATH, './h4').text
+    actualList.append(productName)
     button = result.find_element(By.XPATH, './div/button').click()
+print(actualList)
+assert actualList == expectedList
+    
     
 driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
@@ -37,11 +46,7 @@ totalAmount = int(driver.find_element(By.CLASS_NAME, 'totAmt').text)
 assert sum == totalAmount
 print(f"promoCode: {sum} is equal to totalSum: {totalAmount}")
 
-# Discount amount
-DiscountAmount = int(driver.find_element(By.CLASS_NAME, 'discountAmt').text)
-print(DiscountAmount)
-assert DiscountAmount < totalAmount
-print("Shetty Coupon works")
+
 
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.XPATH, "//button[contains(text(),'Apply')]").click()
@@ -52,4 +57,10 @@ promoInfo = driver.find_element(By.CLASS_NAME, "promoInfo").text
 
 assert promoInfo == "Code applied ..!"
 print("passed")
+
+# Discount amount
+DiscountAmount = float(driver.find_element(By.CLASS_NAME, 'discountAmt').text)
+print(DiscountAmount)
+assert totalAmount > DiscountAmount
+print("Shetty Coupon works")
 
